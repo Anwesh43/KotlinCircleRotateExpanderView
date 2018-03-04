07e2811600f9fcdd68e958403970d6b308c3e366
Anwesh43 <anweshthecool0@gmail.com>
2018-03-04 11:26:51 +0530

    adding CircleRotateExpanderView in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclerotateexpanderview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclerotateexpanderview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincirclerotateexpanderview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circlerotateexpanderview.CircleRotateExpanderView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleRotateExpanderView.create(this)
     }
 }
