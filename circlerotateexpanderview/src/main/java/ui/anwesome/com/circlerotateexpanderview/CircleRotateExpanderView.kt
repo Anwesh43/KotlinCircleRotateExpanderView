7d5adae34ed4bdd85c5fdb08b31837d7096e1760
Anwesh43 <anweshthecool0@gmail.com>
2018-03-04 10:28:37 +0530

    adding CircleRotateExpanderView class

--- circlerotateexpanderview/src/main/java/ui/anwesome/com/circlerotateexpanderview/CircleRotateExpanderView.kt
+++ circlerotateexpanderview/src/main/java/ui/anwesome/com/circlerotateexpanderview/CircleRotateExpanderView.kt
@@ -6,3 +6,17 @@ package ui.anwesome.com.circlerotateexpanderview
 import android.content.*
 import android.graphics.*
 import android.view.*
+class CircleRotateExpanderView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent)  : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
